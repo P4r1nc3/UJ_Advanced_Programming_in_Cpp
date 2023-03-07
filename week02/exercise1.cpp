827e364e02dfd9841dd93e3f40582c4358453915
parince <konrad.tupta@gmail.com>
2023-03-07 09:04:12 +0100

    Completed exercise 1 week 2

---
+++ week02/exercise1.cpp
@@ -0,0 +1,22 @@
+/*
+ * Polimorfizm dynamiczny to mechanizm programowania obiektowego, który pozwala na wykonywanie różnych działań na obiektach różnych klas,
+ * w zależności od ich rzeczywistego typu w momencie wywołania. Oznacza to, że metoda wywołana na obiekcie klasy pochodnej będzie zależna od
+ * konkretnego obiektu, a nie od typu, którego użyto do deklaracji zmiennej.
+*/
+
+/*
+ * Polimorfizm statyczny to mechanizm programowania, który pozwala na wykonywanie różnych działań na obiektach różnych klas
+ * w czasie kompilacji, w zależności od ich deklarowanego typu. Oznacza to, że metoda wywołana na obiekcie klasy pochodnej
+ * będzie zależna od typu, którego użyto do deklaracji zmiennej.
+*/
+
+/*
+ * Klasa abstrakcyjna to klasa, która nie może być bezpośrednio tworzona jako obiekt, ale może być użyta jako podstawa do tworzenia innych klas.
+ * Klasa abstrakcyjna może zawierać metody abstrakcyjne, które muszą być zaimplementowane przez każdą klasę dziedziczącą.
+*/
+
+/*
+ * Funkcja wirtualna to metoda w klasie, która może być przesłonięta przez metody w klasach pochodnych.
+ * Dzięki temu, wywołanie metody wirtualnej na obiekcie klasy pochodnej spowoduje wywołanie odpowiedniej metody w klasie pochodnej,
+ * a nie w klasie bazowej. Funkcje wirtualne są często używane w celu zapewnienia polimorfizmu dynamicznego.
+ */
